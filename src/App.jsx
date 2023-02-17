022a73b6d45b98641f91c147060fccc666dac17d
Miguel Bravo <miguelbravobalestrini@gmail.com>
2023-02-17 16:43:40 +0000

    move images to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,31 @@ import Counter from './components/Counter';
 function App() {
   const pets = [
     {
-      image: 'https://images.wagwalkingweb.com/media/daily_wag/blog_articles/hero/1670938235.1927571/fun-facts-about-labrador-retrievers.jpg',
+      image: './public/labrador.jpeg',
       name: 'Goose',
       breed: 'Labrador',
       borderColor: 'red',
     },
     {
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bulldog_inglese.jpg/1200px-Bulldog_inglese.jpg',
+      image: './public/bulldog.jpeg',
       name: 'Rambo',
       breed: 'Bulldog',
       borderColor: 'blue',
     },
     {
-      image: 'https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg',
+      image: './public/german_shepherd.jpeg',
       name: 'Rex',
       breed: 'German Shepherd',
       borderColor: 'green',
     },
     {
-      image: 'https://image.petmd.com/files/styles/863x625/public/2022-10/beagle-dog.jpg',
+      image: './public/beagle.jpeg',
       name: 'Charlie',
       breed: 'Beagle',
       borderColor: 'yellow',
     },
     {
-      image: 'https://media-be.chewy.com/wp-content/uploads/2021/05/05180433/Cocker-Spaniel_FeaturedImage-1024x615.jpg',
+      image: './public/cocker_spaniel.jpeg',
       name: 'Remi',
       breed: 'Cocker Spaniel',
       borderColor: 'orange',
